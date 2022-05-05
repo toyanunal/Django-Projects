61a8fdeffd90c88110b4099a4f046e2c83ddb09a
toyanunal <toyanunal@gmail.com>
2022-05-05 17:03:47 +0300

    add comma after TEMPLATE_DIR in settings.py file

--- 3-Django_Level_Three/basicforms/basicforms/settings.py
+++ 3-Django_Level_Three/basicforms/basicforms/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'basicforms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
