6d72a776051b0c21f58209e37c2885a4a9362baf
toyanunal <toyanunal@gmail.com>
2022-05-20 08:49:56 +0300

    minor update

--- 1-Django_Level_One/first_project/first_app/views.py
+++ 1-Django_Level_One/first_project/first_app/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 
 def index(request):
     my_dict = {'insert_me':"Hello I am from views.py!"}
-    return render(request,'first_app/index.html',context=my_dict)
+    return render(request, 'first_app/index.html', context=my_dict)
--- 4-Django_Level_Four/learning_templates/learning_templates/urls.py
+++ 4-Django_Level_Four/learning_templates/learning_templates/urls.py
@@ -1,7 +1,7 @@
 """learning_templates URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,7 +18,7 @@ from django.urls import path, include
 from basic_app import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('basic_app/',include('basic_app.urls')),
+    path('basic_app/', include('basic_app.urls')),
 ]
